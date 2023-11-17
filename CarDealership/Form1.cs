0595d4a07b0bdabb3a96f2e682c45aab86485d6c
“Douglas <douglasgray155@gmail.com>
2023-11-17 12:47:46 +0000

    basic SQL access template

---
+++ CarDealership/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarDealership
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\Douglas\source\repos\CarDealership\CarDealership\Cars.mdf; Integrated Security = True";  
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    
+                    connection.Open();
+
+                    string sql = "SELECT * FROM Cars";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                MessageBox.Show(reader.GetString(2));
+                            }
+                        }
+                    }
+
+                    connection.Close();
+                }
+            }
+            catch (SqlException er)
+            {
+                MessageBox.Show("Failed to connect to Database!", er.ToString());
+            }
+        
+
+        }
+    }
+}
