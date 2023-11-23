1a7b212a383c1722f07f9181a520532cfe4d6c46
Douglas-Gray <douglasgray155@gmail.com>
2023-11-23 20:03:13 +0000

    Added class & method to handle SQL Queries

---
+++ CarDealership/QueryHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarDealership
+{
+    internal class QueryHandler
+    {
+        const string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\Douglas\source\repos\CarDealership\CarDealership\Cars.mdf; Integrated Security = True";
+        public string UpdateDB(string sqlStatement) {
+
+            string message = "";
+            int queryCheck; 
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                using (connection)
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(sqlStatement, connection))
+                    {
+
+                        queryCheck = command.ExecuteNonQuery();
+
+                        if (queryCheck == 0)
+                        {
+                            message = "No record found to update";
+                        }
+                        else { message = "Record Updated"; }
+
+                    }
+                    connection.Close();
+                }
+            }
+            catch (SqlException er)
+            {
+                MessageBox.Show("Failed to connect to Database!" + er.ToString());
+                connection.Close();
+            }
+
+            return message; 
+        }
+
+    }
+}
