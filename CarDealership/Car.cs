bc77b8f3c3c35d3f6ce111304bf460a9da2575d9
Douglas-Gray <douglasgray155@gmail.com>
2023-11-24 12:40:19 +0000

    Added object to handle car records

---
+++ CarDealership/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership
+{
+    internal class Car
+    {
+        public string Reg { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public Car(string reg, string brand, string model)
+        {
+            Reg = reg;
+            Brand = brand;
+            Model = model; 
+        }
+
+    }
+}
